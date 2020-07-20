41e8bf8e1c7b7e5f4e7761306290a2b2e2294747
Drew Ross <mail.drew.ross@gmail.com>
2020-07-20 13:47:00 -0700

    Write and pass test for Episodes component.

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,4 +1,17 @@
 import React from 'react';
-import { render, screen, userEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { showMockData } from '../utils/showMockData';
 import Episodes from '../components/Episodes';
 
+const mockEpisodes = showMockData.data._embedded.episodes;
+
+test('The component renders without errors', () => {
+  render(<Episodes episodes={mockEpisodes} />);
+});
+
+test('The component renders episodes to screen', () => {
+  render(<Episodes episodes={mockEpisodes} />);
+  
+  expect(screen.getByText(/Chapter One: The Vanishing of Will Byers/i)).toBeInTheDocument();
+  expect(screen.getByText(/Chapter Eight: The Upside Down/i)).toBeInTheDocument();
+});
