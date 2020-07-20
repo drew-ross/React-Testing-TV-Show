adffccc5bd8e272be7315c4339ca8bb65ea8a4c8
Drew Ross <mail.drew.ross@gmail.com>
2020-07-20 13:29:50 -0700

    Add episodesMockData.js for testing.

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -5,9 +5,5 @@ export const fetchShow = () => {
         .get(
             "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
         )
-        .then(res => {
-            // setShow(res.data);
-            // setSeasons(formatSeasons(res.data._embedded.episodes));
-            return res;
-        });
+        .then(res => res);
 };
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render, screen, userEvent } from '@testing-library/react';
+import Episodes from '../components/Episodes';
+
